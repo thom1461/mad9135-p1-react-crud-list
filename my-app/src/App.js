fdb305c08919501f87fa1eb7f46a7b10cbe5ec48
Sara Thomas <thom1461@algonquinlive.com>
2021-11-02 16:57:27 -0400

    created components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import NewItemView from './NewItemView';
+import ListView from './ListView';
+import AppHeader from './AppHeader';
+import ListItem from '.ListItem';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
